0700754d851e1d00e91f680da6f5af13d7e3880a
Ajay Pathak <APathak2@slb.com>
2019-10-05 07:34:56 +0530

    implemented the code to find first repeating char in a string

---
+++ DataStructure.UnitTest/FirstRepeatedCharacterTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructure.UnitTest
+{
+    [TestClass]
+    public class FirstRepeatedCharacterTests
+    {
+        [TestMethod]
+        public void Happy_Pathak_One_Repeated_Char()
+        {
+            FirstRepeatedCharacter character= new FirstRepeatedCharacter();
+            char expected = 'a';
+            char actual = character.GetFirstRepeatedCharacter("ajay");
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void Happy_Pathak_Zero_Repeated_Char()
+        {
+            FirstRepeatedCharacter character = new FirstRepeatedCharacter();
+            char expected = '\n';
+            char actual = character.GetFirstRepeatedCharacter("Vijay");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DataStructure/FirstRepeatedCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    public class FirstRepeatedCharacter
+    {
+        public char GetFirstRepeatedCharacter(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Empty String");
+            }
+
+            char defaultChar = '\n';
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            char[] chars = input.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var c = chars[i];
+                if (dictionary.ContainsKey(c))
+                {
+                    return c;
+
+                }
+                dictionary.Add(c, i);
+            }
+            return defaultChar;
+
+        }
+    }
+}
